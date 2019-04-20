760c2cb37de4fb751851f74163b008fa7fb9e5c3
Peter Donker <peter@bring2mind.net>
2019-04-20 22:11:10 +0200

    Move selectors into their own object

--- users.js
+++ users.js
@@ -1,5 +1,20 @@
 const names = require("./names");
 
+const elements = {
+  addUserBtn:
+    "#users-container > div > div.boilerplate-app.personaBar-mainContainer > div > div > div.dnn-persona-bar-page-header > div > button",
+  addUserPanel: "div.new-user-box",
+  firstNameTextBox: "div.new-user-box input[type='text'][tabindex='1']",
+  lastNameTextBox: "div.new-user-box input[type='text'][tabindex='2']",
+  userNameTextBox: "div.new-user-box input[type='text'][tabindex='3']",
+  emailTextBox: "div.new-user-box input[type='text'][tabindex='4']",
+  passwordTextBox: "div.new-user-box input[type='password'][tabindex='7']",
+  passwordConfirmTextBox: "div.new-user-box input[type='password'][tabindex='8']",
+  switches: "div.new-user-box .dnn-switch",
+  emailNotificationCheckbox: "div.email-notification-line input",
+  saveNewUserBtn: "div.new-user-box button[role='primary']"
+};
+
 exports.addUser = async function(
   page,
   firstName,
@@ -14,19 +29,17 @@ exports.addUser = async function(
   const frames = await page.frames();
   const personaBar = frames.find(f => f.name() === "personaBar-iframe");
   if (personaBar) {
-    await personaBar.click(
-      "#users-container > div > div.boilerplate-app.personaBar-mainContainer > div > div > div.dnn-persona-bar-page-header > div > button"
-    );
-    await personaBar.waitForSelector("div.new-user-box");
-    await personaBar.click("div.new-user-box input[type='text'][tabindex='1']");
+    await personaBar.click(elements.addUserBtn);
+    await personaBar.waitForSelector(elements.addUserPanel);
+    await personaBar.click(elements.firstNameTextBox);
     await page.keyboard.type(firstName);
-    await personaBar.click("div.new-user-box input[type='text'][tabindex='2']");
+    await personaBar.click(elements.lastNameTextBox);
     await page.keyboard.type(lastName);
-    await personaBar.click("div.new-user-box input[type='text'][tabindex='3']");
+    await personaBar.click(elements.userNameTextBox);
     await page.keyboard.type(userName);
-    await personaBar.click("div.new-user-box input[type='text'][tabindex='4']");
+    await personaBar.click(elements.emailTextBox);
     await page.keyboard.type(email);
-    var switches = await personaBar.$$("div.new-user-box .dnn-switch");
+    var switches = await personaBar.$$(elements.switches);
     if (switches && switches.length == 2) {
       if (!authorized) {
         await switches[0].click();
@@ -34,21 +47,23 @@ exports.addUser = async function(
       if (randomPw) {
         await switches[1].click();
       } else {
-        await personaBar.click(
-          "div.new-user-box input[type='password'][tabindex='7']"
-        );
+        await personaBar.click(elements.passwordTextBox);
         await page.keyboard.type(password);
-        await personaBar.click(
-          "div.new-user-box input[type='password'][tabindex='8']"
-        );
+        await personaBar.click(elements.passwordConfirmTextBox);
         await page.keyboard.type(password);
       }
     }
     if (sendEmail) {
-      await personaBar.click("div.email-notification-line input");
+      await personaBar.click(elements.emailNotificationCheckbox);
     }
-    await personaBar.click("div.new-user-box button[role='primary']");
+    await personaBar.click(elements.saveNewUserBtn);
     await page.waitFor(5000); // wait for popup to fade
+  }
+};
+
+exports.addRandomUsers = async function(page, nrUsers, password) {
+  for (var i = 0; i < nrUsers; i++) {
+    await this.addRandomUser(page, true, password);
   }
 };
 
@@ -77,7 +92,10 @@ exports.getRandomUserName = function() {
     firstName: fn,
     lastName: ln,
     email: fn.toLowerCase() + "@" + ln.toLowerCase() + ".name",
-    userName: fn.toLowerCase().substring(0, 1) + ln.toLowerCase() + Math.floor(Math.random() * 100).toString(),
+    userName:
+      fn.toLowerCase().substring(0, 1) +
+      ln.toLowerCase() +
+      Math.floor(Math.random() * 100).toString(),
     displayName: fn + " " + ln
   };
 };
